7ecada39ea4106ce4f8cdf127ef718013497c964
satya prakash <satya200prakash@gmail.com>
2025-03-18 22:33:15 +0530

    update user details

--- 07reactRouter/src/components/Footer/Footer.jsx
+++ 07reactRouter/src/components/Footer/Footer.jsx
@@ -38,7 +38,7 @@ return (
             <ul className="text-gray-500 font-medium">
                 <li className="mb-4">
                     <a
-                        href="https://github.com/hiteshchoudhary"
+                        href="https://github.com/sps9938"
                         className="hover:underline"
                         target="_blank"
                         rel="noreferrer"
@@ -75,7 +75,7 @@ return (
     <span className="text-sm text-gray-500 sm:text-center">
         © 2023
         <a href="https://hiteshchoudhary.com/" className="hover:underline">
-            hiteshchoudhary
+            satya sahu
         </a>
         . All Rights Reserved.
     </span>
